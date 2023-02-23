078f16a1903c24b6894dadc3ac5426e1dc8006c7
devinmuzzy <68883649+devinmuzzy@users.noreply.github.com>
2023-02-22 20:40:10 -0800

    initial commit

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Home</h1>"
+
+
+
+# flask examples
+# #/api/blue?name=name
+# @app.route("/api/blue", methods=['GET'])
+# def blue():
+#     if request.method != 'GET':
+#         return "<script>alert('invalid method')</script>"
+#     elif 'name' in request.args:
+#         return f"<h1 style='color:blue'>BlueServer says hello {request.args['name']}!</h1>"
+#     else:
+#         return "<script>alert('invalid query string')</script>"
+
+# #/api/blue?name=name
+# @app.route("/api/jsonBlue", methods=['POST'])
+# def jsonBlue():
+#     content_type = request.headers.get('Content-Type')
+#     if (content_type == 'application/json'):
+#         json = request.json
+#         json['visitedBlue'] = 'true'
+#         return json
+#     else:
+#         return 'Content-Type not supported!'
+
+
+
+
+# if __name__ == '__main__':
+#     app.run(debug=True, port=5001)
