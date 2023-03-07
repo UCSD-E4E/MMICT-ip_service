9f8ae30ce91535e40b40d416b25b83ea15a9bad9
Leon Lin <lianglin.021@gmail.com>
2023-03-06 22:13:14 -0800

    config file for S3 credentials

--- service/S3ImgGetter.py
+++ service/S3ImgGetter.py
@@ -1,11 +1,12 @@
+import configparser
 import os.path
 
 import boto3
 
-access_key = ''
-secret_key = ''
-bucket_name = ''
-image_folder = './'
+image_folder = 'images'
+config_file = 'service/S3Access.ini'
+config_section = 'AWS_S3'
+download_img = 'output.tif'
 
 
 def getImg(img_ref):
@@ -19,9 +20,16 @@ def getImg(img_ref):
 
 
 def downloadImg(img_ref):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+    s3_section = config[config_section]
+    access_key = s3_section['aws_access_key_id']
+    secret_key = s3_section['aws_secret_access_key']
+    bucket_name = s3_section['bucket_name']
+
     s3_client = boto3.client(
         's3',
         aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key
+        aws_secret_access_key=secret_key,
     )
-    s3_client.download_file(bucket_name, 'example_input_image.tif', os.path.join(image_folder, 'test.tif'))
+    s3_client.download_file(bucket_name, img_ref, os.path.join(image_folder, download_img))
