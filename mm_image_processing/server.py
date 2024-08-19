7075381f7f7bec70dec928f4a4d6f5f9fc105203
zclawr <zclawrence@ucsd.edu>
2024-08-19 14:06:16 -0700

    removed s3 secrets, cleaned for deployment

--- mm_image_processing/server.py
+++ mm_image_processing/server.py
@@ -68,127 +68,131 @@ def ws_process(ws):
         6. Builds and returns GeoJSON representation from the classified data.
 
     """
+    while (True):
+        app.logger.debug("received request")
 
-    app.logger.debug("received request")
+        # recieve the websocket data
+        data = ws.receive()
+        app.logger.debug("validating request: " + data)
 
-    # recieve the websocket data
-    data = ws.receive()
-    app.logger.debug("validating request: " + data)
+        # validate request matches expected json format
+        if not validate_process_request_json(data):
+            ws.send('invalid request, killing connection')
+            ws.close(1)
+            return
 
-    # validate request matches expected json format
-    if not validate_process_request_json(data):
-        ws.send('invalid request, killing connection')
-        ws.close(1)
-        return
+        # now that it is known to be "safe" load json object, accept the request
+        request_json = json.loads(data, strict=False)
+        rgb_img_ref = request_json['rgb_image_ref']
+        nir_img_ref = request_json['nir_image_ref']
+        date_of_capture = request_json['date_of_capture']
+        location = request_json['location']
+        r_channel = request_json['r_channel']
+        g_channel = request_json['g_channel']
+        b_channel = request_json['b_channel']
+        nir_channel = request_json['nir_channel']
 
-    # now that it is known to be "safe" load json object, accept the request
-    request_json = json.loads(data, strict=False)
-    rgb_img_ref = request_json['rgb_image_ref']
-    nir_img_ref = request_json['nir_image_ref']
-    date_of_capture = request_json['date_of_capture']
-    location = request_json['location']
-    r_channel = request_json['r_channel']
-    g_channel = request_json['g_channel']
-    b_channel = request_json['b_channel']
-    nir_channel = request_json['nir_channel']
-
-    # write back progress update on the websocket
-    progress = {"status": "Accepted classification request", "percent": 5}
-    msg = json.dumps(progress)
-    ws.send(msg)
-
-    # write back progress again
-    progress = {"status": "Downloading specified imagery", "percent": 10}
-    msg = json.dumps(progress)
-    ws.send(msg)
-
-    # try to download the image at rgb_img_ref and nir_img_ref from the s3 bucket
-
-    # Note: For the dummy-upload branch, the file being downloaded is NOT the one being returned back, this
-    # is simply a sanity check to ensure the download is working properly. It may be valuable to keep this 
-    # to ensure any production-ready code is playing nicely with S3 as expected.
-    try:
-        rgbImgUrl = getImg(rgb_img_ref, app)
-        nirImgUrl = getImg(nir_img_ref, app)
-    except Exception as e:
-        app.logger.error(e)
-        ws.close(1)
-        return
-    
-    # write back progress again
-    progress = {"status": "Preparing to preprocess imagery", "percent": 15}
-    msg = json.dumps(progress)
-    ws.send(msg)
-
-    # Currently, all processing is skipped in the pipeline for the purposes of demoing the GEOJSon transfer between services
-    # as the processing has some issues that I'm currently resolving. The architecture between the services is complete, however.
-    # -------------------------------------------------------------
-    
-    # # process the image now that it is downloaded locally
-    # img_shape, bbox, processed_array = processImgFromLocal(imgUrl, app)
-
-    # # write back progress update
-    # progress["status"] = "CLASSIFYING"
-    # # progress["bbox"] = bbox # write bbox back for debugging purpose only
-    # msg = json.dumps(progress)
-    # ws.send(msg)
-
-    # # call the classify() method to get classified data from classification_service
-    # # * asyncio set so this route does not need to be an async function *
-    # asyncio.set_event_loop(asyncio.new_event_loop())
-    # classified_array = asyncio.get_event_loop().run_until_complete(
-    #     classify(classifier_id, processed_array))  # NOTE use await instead of this since it is cleaner
-
-    # # check if classify() method failed
-    # if classified_array is None:
-    #     ws.send('classification_service error')
-    #     ws.close(1)
-    #     return
-    
-
-    # # write back progress update
-    # progress["status"] = "BUILDING"
-    # msg = json.dumps(progress)
-    # ws.send(msg)
-
-    # # build geojson from classified data, then convert to a serializable json format
-    # geojson_raw = build_geojson(img_shape, bbox, classified_array)
-    # geojson = geojson_raw.to_json()
-
-    try:
-        f = open('mm_image_processing/labels.json') # working directory is at /ip-service, as defined in the Dockerfile
-    except Exception as e:
-        app.logger.error(e)
-        print(os.getcwd())
-        ws.close(1)
-        return
-    
-    geojson = json.load(f)
-    progress = {"status": "Compressing geodata", "percent": 70}
-    msg = json.dumps(progress)
-    ws.send(msg)
-    compressed = zlib.compress(json.dumps(geojson).encode(), 3)
-
-    progress = {"status": "Receiving geodata", "percent": 80}
-    progress["geojson_flag"] = "sending"    
-    msg = json.dumps(progress)
-    ws.send(msg)
-
-    geojson_chunks = chunk_geojson(json.dumps(geojson)) # chunk_geojson(str(compressed))
-    progress_increment = 20 / len(geojson_chunks)
-    for i in range(len(geojson_chunks)):
-        progress["geojson_chunk"] = geojson_chunks[i]
-        progress["percent"] = round(80 + (i * progress_increment), 2)
+        # write back progress update on the websocket
+        progress = {"status": "Accepted classification request", "percent": 5}
         msg = json.dumps(progress)
         ws.send(msg)
-    progress["geojson_flag"] = "done"
-    progress["status"] = "Completed successfully"
-    progress["percent"] = 100
-    msg = json.dumps(progress)
-    
-    # final writeback on websocket, then close connection
-    ws.send(msg)
-    ws.close()
+
+        # write back progress again
+        progress = {"status": "Downloading specified imagery", "percent": 10}
+        msg = json.dumps(progress)
+        ws.send(msg)
+
+        # try to download the image at rgb_img_ref and nir_img_ref from the s3 bucket
+
+        # Note: For the dummy-upload branch, the file being downloaded is NOT the one being returned back, this
+        # is simply a sanity check to ensure the download is working properly. It may be valuable to keep this 
+        # to ensure any production-ready code is playing nicely with S3 as expected.
+        try:
+            rgbImgUrl = getImg(rgb_img_ref, app)
+            nirImgUrl = getImg(nir_img_ref, app)
+        except Exception as e:
+            app.logger.error(e)
+            ws.close(1)
+            return
+        
+        # write back progress again
+        progress = {"status": "Preparing to preprocess imagery", "percent": 15}
+        msg = json.dumps(progress)
+        ws.send(msg)
+
+        # Currently, all processing is skipped in the pipeline for the purposes of demoing the GEOJSon transfer between services
+        # as the processing has some issues that I'm currently resolving. The architecture between the services is complete, however.
+        # -------------------------------------------------------------
+        
+        # # process the image now that it is downloaded locally
+        # img_shape, bbox, processed_array = processImgFromLocal(imgUrl, app)
+
+        # # write back progress update
+        # progress["status"] = "CLASSIFYING"
+        # # progress["bbox"] = bbox # write bbox back for debugging purpose only
+        # msg = json.dumps(progress)
+        # ws.send(msg)
+
+        # # call the classify() method to get classified data from classification_service
+        # # * asyncio set so this route does not need to be an async function *
+        # asyncio.set_event_loop(asyncio.new_event_loop())
+        # classified_array = asyncio.get_event_loop().run_until_complete(
+        #     classify(classifier_id, processed_array))  # NOTE use await instead of this since it is cleaner
+
+        # # check if classify() method failed
+        # if classified_array is None:
+        #     ws.send('classification_service error')
+        #     ws.close(1)
+        #     return
+        
+
+        # # write back progress update
+        # progress["status"] = "BUILDING"
+        # msg = json.dumps(progress)
+        # ws.send(msg)
+
+        # # build geojson from classified data, then convert to a serializable json format
+        # geojson_raw = build_geojson(img_shape, bbox, classified_array)
+        # geojson = geojson_raw.to_json()
+
+        try:
+            f = open('mm_image_processing/labels.json') # working directory is at /ip-service, as defined in the Dockerfile
+        except Exception as e:
+            app.logger.error(e)
+            print(os.getcwd())
+            ws.close(1)
+            return
+        
+        geojson = json.load(f)
+        progress = {"status": "Compressing geodata", "percent": 70}
+        msg = json.dumps(progress)
+        ws.send(msg)
+        compressed = zlib.compress(json.dumps(geojson).encode(), 4)
+
+        progress = {"status": "Receiving geodata", "percent": 80}
+        progress["geojson_flag"] = "sending"    
+        msg = json.dumps(progress)
+        ws.send(msg)
+
+        # progress["geojson_compressed"] = compressed
+        # msg = json.dumps(progress)
+        # ws.send(msg)
+
+        geojson_chunks = chunk_geojson(json.dumps(geojson)) # chunk_geojson(str(compressed))
+        progress_increment = 20 / len(geojson_chunks)
+        for i in range(len(geojson_chunks)):
+            progress["geojson_chunk"] = geojson_chunks[i]
+            progress["percent"] = round(80 + (i * progress_increment), 2)
+            msg = json.dumps(progress)
+            ws.send(msg)
+        progress["geojson_flag"] = "done"
+        progress["status"] = "Completed successfully"
+        progress["percent"] = 100
+        msg = json.dumps(progress)
+        
+        # final writeback on websocket, then close connection
+        ws.send(msg)
+        # ws.close()
 
 def chunk_geojson(geojson_str):
     msg = geojson_str
--- mm_image_processing/util/s3_img_getter.py
+++ mm_image_processing/util/s3_img_getter.py
@@ -8,8 +8,6 @@ from dotenv import load_dotenv
 load_dotenv()
 # Load S3 credentials from .env (defined in Docker compose)
 bucket_name = os.getenv("AWS_BUCKET_NAME")
-access_key = os.getenv("AWS_ACCESS_KEY_ID")
-secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 # For no folder in S3 bucket, use the following
 image_folder = ''
@@ -31,11 +29,7 @@ def getImg(img_ref, app):
     # secret_key = s3_section['aws_secret_access_key']
     # bucket_name = s3_section['bucket_name']
 
-    s3_client = boto3.client(
-        's3',
-        aws_access_key_id=access_key,
-        aws_secret_access_key=secret_key,
-    )
+    s3_client = boto3.client('s3')
     
     s3_client.download_file(bucket_name, img_ref, imgUrl)
 
